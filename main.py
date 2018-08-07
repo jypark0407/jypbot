ac04354596bdcefff075cdbb0fa2de6c92e56361
Jinyoung Park <jypark04070@gmail.com>
2018-08-07 15:19:09 +0900

    connect to Slack server using python-rtmbot

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from rtmbot import RtmBot
+
+config = {
+    "SLACK_TOKEN": "xoxp-332590185332-387288594384-413270493366-602dc3fcd852e1cb7ce28502cf0ac95c",
+    "ACTIVE_PLUGINS": []
+}
+bot = RtmBot(config)
+bot.start()
+
 print("안냥")
 print("웅 앵 냥")
 print("야옹 야옹")
