44ce23e0b410b5e52f2515e281c764d72b7abf35
Jinyoung Park <jypark04070@gmail.com>
2018-08-07 16:09:39 +0900

    add .gitignore and pipfiles

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from rtmbot import RtmBot
+from rtmbot.core import Plugin
+import re
+import random
+import secret
+
+response = ["안냥", "웅 앵 냥", "야옹야옹", "냥?", "나는귀엽냥", ":cat: :cat: :cat:", "너 내집사가 돼랑"]
+random_response = random.sample(response, 1)[0]
+
+class HelloPlugin(Plugin):
+    def process_message(self,data):
+        if re.match(r'.*냥.*', data["text"]):
+            self.outputs.append([data["channel"], random_response)
 
 config = {
-    "SLACK_TOKEN": "xoxp-332590185332-387288594384-413270493366-602dc3fcd852e1cb7ce28502cf0ac95c",
-    "ACTIVE_PLUGINS": []
+    "SLACK_TOKEN": secret.SLACK_TOKEN,
+    "ACTIVE_PLUGINS": ["main.HelloPlugin"]
 }
 bot = RtmBot(config)
 bot.start()
 
+
 print("안냥")
 print("웅 앵 냥")
-print("야옹 야옹")
+print("야옹야옹")
 print("냥?")
+print("나는귀엽냥")
+print(":cat: :cat: :cat:")
